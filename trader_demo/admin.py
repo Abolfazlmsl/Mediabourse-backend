54f5acdf18924639af1bff2912a013383e028e2d
a.rasti <ali.rasti1995@gmail.com>
2020-08-09 10:48:05 +0430

    take demo files back

--- trader_demo/admin.py
+++ trader_demo/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from . import models
+from .models import *
 
 # Register your models here.
 
-
-# admin.site.register(models.Company)
+admin.site.register(Company)
+admin.site.register(Category)
+admin.site.register(Watcher)
+admin.site.register(Stock)
+admin.site.register(Orders)
+admin.site.register(Cart)
