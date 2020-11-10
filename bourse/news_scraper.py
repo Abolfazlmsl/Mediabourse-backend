97006c4d3cf336b0b256d2ba5bbc2a34c538c3e0
Amirhossein <amirhossein_barari76@yahoo.com>
2020-11-10 14:53:17 +0330

    news scrapper modify

--- bourse/news_scraper.py
+++ bourse/news_scraper.py
@@ -11,9 +11,9 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 urls = [
-    'http://www.fipiran.com/News?Cat=1&Feeder=0',
-    'http://www.fipiran.com/News?Cat=2&Feeder=0',
-    'http://www.fipiran.com/News?Cat=4&Feeder=0',
+    # 'http://www.fipiran.com/News?Cat=1&Feeder=0',
+    # 'http://www.fipiran.com/News?Cat=2&Feeder=0',
+    # 'http://www.fipiran.com/News?Cat=4&Feeder=0',
     'http://www.fipiran.com/News?Cat=5&Feeder=0',
 ]
 
@@ -45,11 +45,12 @@ def scraper():
                     title = soup.find('h1').get_text()
                     summary = soup.find('div', {'class': 'news-lead'}).get_text()
                     text_list = soup.find('div', {'class': 'news-text'}).find_all('p')
+                    print(text_list)
                     text_list.pop()
                     text_list.pop()
-                    text = {}
+                    text = list()
                     for i, j in enumerate(text_list):
-                        text[i + 1] = j.get_text()
+                        text.append(j)
                     # short_title = soup.find('div', {'class': 'short-title'}).get_text()
 
                     json_list = {
@@ -78,7 +79,7 @@ def scraper():
                             date=unidecode(date_str+time_str)
                         )
                     except IntegrityError:
-                        continue
+                        return
                     # with open('data1.json', 'w', encoding='utf-8') as f:
                     #     json.dump(json_list, f, ensure_ascii=False)
                 except AttributeError:
@@ -86,12 +87,16 @@ def scraper():
 
             elif 'isna.ir' in page_url:
                 try:
-                    date = soup.find('li').find('span', {'class': 'text-meta'})
+                    date = soup.select_one('span.text-meta').get_text()
                     title = soup.find('div', {'class': 'item-title'}).find('h1', {'class': 'first-title'}).get_text()
                     image = soup.find('figure', {'class': 'item-img'}).find('img')['src']
                     summary = soup.find('p', {'class': 'summary'}).get_text()
-                    text = soup.find('div', {'class': 'item-text'}).get_text()
-
+                    text = soup.find('div', {'class': 'item-text'}).find_all('p')
+                    print(text, 'sfaeqefefewf')
+                    text.pop()
+                    text.pop()
+                    text.pop()
+                    print(text, 'bah bha')
                     json_list = {
                         "title": title,
                         "image": image,
@@ -100,8 +105,42 @@ def scraper():
                         "date": date,
                     }
                     print(json_list)
-                    # with open('data3.json', 'w', encoding='utf-8') as f:
-                    #     json.dump(json_list, f, ensure_ascii=False)
+                    date, time = date.split('/')
+                    time_str = ''.join(time.split(':'))
+                    date = date.split(' ')
+                    date = date[0:3]
+                    month_number = {
+                        'فروردین': '01',
+                        'اردیبهشت': '02',
+                        'خرداد': '03',
+                        'تیر': '04',
+                        'مرداد': '05',
+                        'شهریور': '06',
+                        'مهر': '07',
+                        'آبان': '08',
+                        'آذر': '09',
+                        'دی': '10',
+                        'بهمن': '11',
+                        'اسفند': '12',
+                    }
+                    date[1] = month_number[date[1]]
+                    if len(date[0]) == 1:
+                        date[0] = '0'+str(date[0])
+                    date_str = date[2]+date[1]+date[0]
+                    datetime_str = unidecode(date_str+time_str).replace(" ", "")
+                    # print('daaaaaaaaaaaaaaaaaaaate', date_str, time_str, datetime_str)
+
+                    try:
+                        News.objects.create(
+                            title=title,
+                            short_description=summary,
+                            description=text,
+                            pic=image,
+                            date=datetime_str
+                        )
+                    except IntegrityError:
+                        return
+
                 except AttributeError:
                     continue
 
